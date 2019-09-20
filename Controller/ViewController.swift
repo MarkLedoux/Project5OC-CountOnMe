afbf783de854669cd2e0716b1c510f3f0a2ae9c9
vinceled <mark.ledoux@live.fr>
2019-09-20 14:53:04 +0200

    creating instance of CountOnMe at line 15 of the view controller

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -11,8 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
+
+    var countOnMe = CountOnMe()
     
-    
+    // View Life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
     // View actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else {
@@ -58,29 +65,6 @@ class ViewController: UIViewController {
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
-        
-        // Create local copy of operations
-        var operationsToReduce = elements
-        
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            let left = Int(operationsToReduce[0])!
-            let operand = operationsToReduce[1]
-            let right = Int(operationsToReduce[2])!
-            
-            let result: Int
-            switch operand {
-            case "+": result = left + right
-            case "-": result = left - right
-            default: fatalError("Unknown operator !")
-            }
-            
-            operationsToReduce = Array(operationsToReduce.dropFirst(3))
-            operationsToReduce.insert("\(result)", at: 0)
-        }
-        
-        textView.text.append(" = \(operationsToReduce.first!)")
-    }
 
 }
 
--- Model/CountOnMe.swift
+++ Model/CountOnMe.swift
@@ -31,10 +31,29 @@ class CountOnMe {
         return textView.text.firstIndex(of: "=") != nil
     }
 
-    // View Life cycles
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    func whenButtonIsTapped() {
+
+        // Create local copy of operations
+            var operationsToReduce = elements
+
+            // Iterate over operations while an operand still here
+            while operationsToReduce.count > 1 {
+                let left = Int(operationsToReduce[0])!
+                let operand = operationsToReduce[1]
+                let right = Int(operationsToReduce[2])!
+
+                let result: Int
+                switch operand {
+                case "+": result = left + right
+                case "-": result = left - right
+                default: fatalError("Unknown operator !")
+                }
+
+                operationsToReduce = Array(operationsToReduce.dropFirst(3))
+                operationsToReduce.insert("\(result)", at: 0)
+            }
+
+            textView.text.append(" = \(operationsToReduce.first!)")
+        }
     
 }
