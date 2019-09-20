f42d77a3daf844e06ffb639402f14e61d9849ff5
vinceled <mark.ledoux@live.fr>
2019-09-20 14:48:55 +0200

    moving the  properties between line 15-40 from the view controller to the model of the app

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -12,33 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
     
-    var elements: [String] {
-        return textView.text.split(separator: " ").map { "\($0)" }
-    }
-    
-    // Error check computed variables
-    var expressionIsCorrect: Bool {
-        return elements.last != "+" && elements.last != "-"
-    }
-    
-    var expressionHaveEnoughElement: Bool {
-        return elements.count >= 3
-    }
-    
-    var canAddOperator: Bool {
-        return elements.last != "+" && elements.last != "-"
-    }
-    
-    var expressionHaveResult: Bool {
-        return textView.text.firstIndex(of: "=") != nil
-    }
-    
-    // View Life cycles
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
     
     // View actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
--- Model/CountOnMe.swift
+++ Model/CountOnMe.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class CountOnMe {
+
+    var elements: [String] {
+        return textView.text.split(separator: " ").map { "\($0)" }
+    }
+
+    // Error check computed variables
+    var expressionIsCorrect: Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+
+    var expressionHaveEnoughElement: Bool {
+        return elements.count >= 3
+    }
+
+    var canAddOperator: Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+
+    var expressionHaveResult: Bool {
+        return textView.text.firstIndex(of: "=") != nil
+    }
+
+    // View Life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
