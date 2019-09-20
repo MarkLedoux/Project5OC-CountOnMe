296806002125b16b589fb92a45d3f198e231af88
vinceled <mark.ledoux@live.fr>
2019-09-20 17:20:17 +0200

    undid the modification i add made on the model and the view controller

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -11,22 +11,42 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
-
-    var countOnMe = CountOnMe()
+    
+    var elements: [String] {
+        return textView.text.split(separator: " ").map { "\($0)" }
+    }
+    
+    // Error check computed variables
+    var expressionIsCorrect: Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+    
+    var expressionHaveEnoughElement: Bool {
+        return elements.count >= 3
+    }
+    
+    var canAddOperator: Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+    
+    var expressionHaveResult: Bool {
+        return textView.text.firstIndex(of: "=") != nil
+    }
     
     // View Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
     // View actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else {
             return
         }
         
-        if countOnMe.expressionHaveResult {
+        if expressionHaveResult {
             textView.text = ""
         }
         
@@ -34,7 +54,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tappedAdditionButton(_ sender: UIButton) {
-        if countOnMe.canAddOperator {
+        if canAddOperator {
             textView.text.append(" + ")
         } else {
             let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
@@ -44,7 +64,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tappedSubstractionButton(_ sender: UIButton) {
-        if countOnMe.canAddOperator {
+        if canAddOperator {
             textView.text.append(" - ")
         } else {
             let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
@@ -54,17 +74,40 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tappedEqualButton(_ sender: UIButton) {
-        guard countOnMe.expressionIsCorrect else {
+        guard expressionIsCorrect else {
             let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
         
-        guard countOnMe.expressionHaveEnoughElement else {
+        guard expressionHaveEnoughElement else {
             let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
+        
+        // Create local copy of operations
+        var operationsToReduce = elements
+        
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 {
+            let left = Int(operationsToReduce[0])!
+            let operand = operationsToReduce[1]
+            let right = Int(operationsToReduce[2])!
+            
+            let result: Int
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            default: fatalError("Unknown operator !")
+            }
+            
+            operationsToReduce = Array(operationsToReduce.dropFirst(3))
+            operationsToReduce.insert("\(result)", at: 0)
+        }
+        
+        textView.text.append(" = \(operationsToReduce.first!)")
+    }
 
 }
 
--- Model/CountOnMe.swift
+++ Model/CountOnMe.swift
@@ -9,51 +9,5 @@
 import Foundation
 
 class CountOnMe {
-
-    var elements: [String] {
-        return textView.text.split(separator: " ").map { "\($0)" }
-    }
-
-    // Error check computed variables
-    var expressionIsCorrect: Bool {
-        return elements.last != "+" && elements.last != "-"
-    }
-
-    var expressionHaveEnoughElement: Bool {
-        return elements.count >= 3
-    }
-
-    var canAddOperator: Bool {
-        return elements.last != "+" && elements.last != "-"
-    }
-
-    var expressionHaveResult: Bool {
-        return textView.text.firstIndex(of: "=") != nil
-    }
-
-    func whenButtonIsTapped() {
-
-        // Create local copy of operations
-            var operationsToReduce = elements
-
-            // Iterate over operations while an operand still here
-            while operationsToReduce.count > 1 {
-                let left = Int(operationsToReduce[0])!
-                let operand = operationsToReduce[1]
-                let right = Int(operationsToReduce[2])!
-
-                let result: Int
-                switch operand {
-                case "+": result = left + right
-                case "-": result = left - right
-                default: fatalError("Unknown operator !")
-                }
-
-                operationsToReduce = Array(operationsToReduce.dropFirst(3))
-                operationsToReduce.insert("\(result)", at: 0)
-            }
-
-            textView.text.append(" = \(operationsToReduce.first!)")
-        }
     
 }
