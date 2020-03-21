83d0cfe5c2b2ef74d47af70d4cc050cb8dfc5f2d
vinceled <mark.ledoux@live.fr>
2020-03-21 20:14:41 +0100

    changing the model notification file on the Operator extension

--- Model/Operators.swift
+++ Model/Operators.swift
@@ -8,25 +8,9 @@
 
 import Foundation
 
-enum Operators: CaseIterable {
-    case substraction
-    case addition
-    case division
-    case multiplication
-}
-
-extension Operators {
-    /// Extension explaining what the program needs to return depending on the error return by the program
-    public var operatorDescription: String? {
-        switch self {
-        case .substraction:
-            return NSLocalizedString("-", comment: "")
-        case .addition:
-            return NSLocalizedString("+", comment: "")
-        case .division:
-            return NSLocalizedString("÷", comment: "")
-        case .multiplication:
-            return NSLocalizedString("×", comment: "")
-        }
-    }
+enum Operators: String, CaseIterable {
+    case substraction = "-"
+    case addition = "+"
+    case division = "÷"
+    case multiplication = "×"
 }
